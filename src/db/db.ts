e7cf7120c3cc39ceda8c6b4801e0ce964dded4fc
Vigan Mustafa <48414543+viganm@users.noreply.github.com>
2023-01-24 23:34:01 +0100

    SHOP-1 db changes

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,27 +10,3 @@ export const pool = new Pool({
   password: process.env.PASSWORD,
   port: 5432,
 });
-
-// export const pool = new Pool({
-//   host: "localhost",
-//   port: 5432,
-//   user: "postgres",
-//   password: "Santanotreal2018",
-//   database: "shop-db",
-// });
-
-// pool.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to the database: ", err.stack);
-//   } else {
-//     console.log("Connected to the database.");
-//   }
-// });
-
-// module.exports = pool;
-
-// user: "postgres",
-// host: "localhost",
-// database: "shop-db",
-// password: "Santanotreal2018",
-// port: 5432,
