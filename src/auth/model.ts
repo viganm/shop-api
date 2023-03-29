11f33aee40f1c8077897a338e786ef349cd3aea4
viganmustafa1 <vigan.mustafa@ndbit.net>
2023-03-29 13:38:10 +0200

    SHOP-5 JWT and new login functions

--- src/auth/model.ts
+++ src/auth/model.ts
@@ -1,11 +1,13 @@
 import { pool } from '../db/db';
+import bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
-export const getUser = (personEmail: string, password: string) => {
+export const getPerson = (personEmail: string) => {
   return new Promise((resolve, reject) => {
     const query = {
       text: `SELECT * FROM person
-        WHERE person.personEmail = $1 AND users.userpassword = $2`,
-      values: [personEmail, password],
+        WHERE person.personEmail = $1`,
+      values: [personEmail],
     };
     pool.query(query, (error, result) => {
       if (error) {
@@ -17,29 +19,24 @@ export const getUser = (personEmail: string, password: string) => {
   });
 };
 
-export const getUserWithLocationInfo = (personEmail: string, password: string) => {
-  return new Promise((resolve, reject) => {
-    const query = {
-      text: `SELECT * FROM users
-        LEFT JOIN user_role 
-        ON users.user_id = user_role.user_id
-        LEFT JOIN role
-        ON user_role.role_id = role.role_id
-        LEFT JOIN person
-        ON users.person_id = person.person_id
-        LEFT JOIN person_location
-        ON person.person_id = person_location.person_id
-        LEFT JOIN location
-        ON person_location.location_id = location.location_id
-        WHERE lower(person.personEmail) = lower($1) AND person.password = $2`,
-      values: [personEmail, password],
-    };
-    pool.query(query, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
-  });
+export const comparePasswords = async (password: string, hash: string): Promise<boolean> => {
+  try {
+    const passwordsMatch: boolean = await bcrypt.compare(password, hash);
+    return passwordsMatch;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Error comparing passwords');
+  }
+};
+
+export const generateJwtToken = (user: any): string => {
+  const payload = { userId: user.id };
+  const secret = process.env.JWT_SECRET || 'default_secret';
+
+  const options: jwt.SignOptions = {
+    expiresIn: '1d'
+  };
+
+  const token: string = jwt.sign(payload, secret, options);
+  return token;
 };
