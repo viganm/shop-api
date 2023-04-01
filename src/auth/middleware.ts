5ed295fb966db57bfe95909f3557748b7bf568a1
viganm <vigan.mustafa.vm@gmail.com>
2023-04-01 12:51:22 +0200

    SHOP-5 JWT fixed to get person_id, JWT_SECRET from .env and expiration time

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,18 +1,21 @@
-import { NextFunction, Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
-// import basicAuth from 'basic-auth';
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
 
-export const authorization = (req: Request, res: Response, next: NextFunction) => {
+export const authorization = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = <string>req.cookies ? req.cookies.token : null;
   if (!token) {
-    res.status(403).json({ error: 'Unauthorized.' });
+    res.status(403).json({ error: "Unauthorized." });
     return;
   }
   try {
-    const data = <any>jwt.verify(token, process.env.JWT_KEY || '');
+    const data = <any>jwt.verify(token, process.env.JWT_SECRET || "");
     next();
   } catch (_) {
-    res.status(403).json({ error: 'Unauthorized.' });
+    res.status(403).json({ error: "Unauthorized." });
     return;
   }
 };
--- src/auth/model.ts
+++ src/auth/model.ts
@@ -1,6 +1,6 @@
-import { pool } from '../db/db';
-import bcrypt from 'bcrypt';
-import * as jwt from 'jsonwebtoken';
+import { pool } from "../db/db";
+import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 
 export const getPerson = (personEmail: string) => {
   return new Promise((resolve, reject) => {
@@ -19,22 +19,25 @@ export const getPerson = (personEmail: string) => {
   });
 };
 
-export const comparePasswords = async (password: string, hash: string): Promise<boolean> => {
+export const comparePasswords = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
   try {
     const passwordsMatch: boolean = await bcrypt.compare(password, hash);
     return passwordsMatch;
   } catch (err) {
     console.log(err);
-    throw new Error('Error comparing passwords');
+    throw new Error("Error comparing passwords");
   }
 };
 
-export const generateJwtToken = (user: any): string => {
-  const payload = { userId: user.id };
-  const secret = process.env.JWT_SECRET || 'default_secret';
+export const generateJwtToken = (person: any): string => {
+  const payload = { person_id: person.person_id };
+  const secret = process.env.JWT_SECRET || "default_secret";
 
   const options: jwt.SignOptions = {
-    expiresIn: '1d'
+    expiresIn: "1d",
   };
 
   const token: string = jwt.sign(payload, secret, options);
