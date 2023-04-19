df0af4ef4fb4b77ff44c731c2bb89e15d9ce1035
viganm <vigan.mustafa.vm@gmail.com>
2023-04-19 20:55:35 +0200

    master authorization added on routes of product

--- src/product/route.ts
+++ src/product/route.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import * as productController from "./controller";
+import { authorization } from "../auth/middleware";
 
 const router = Router();
 
 router.get("/products", productController.getProducts);
-router.post("/product", productController.addProducts);
-router.put("/product/:productId", productController.updateProduct);
-router.delete("/product/:productId", productController.deleteProduct);
+router.post("/product", authorization, productController.addProducts);
+router.put(
+  "/product/:productId",
+  authorization,
+  productController.updateProduct
+);
+router.delete(
+  "/product/:productId",
+  authorization,
+  productController.deleteProduct
+);
 
 export default router;
